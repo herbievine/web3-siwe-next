993bba15943556f68386a2af9a32b57abe07c249
Herbie Vine <vineherbie@gmail.com>
2023-02-27 22:55:06 +0100

    fix and funny title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,22 +10,16 @@ const Home: NextPage<HomeProps> = () => {
 
   return (
     <div className="flex h-screen w-full items-center justify-center">
-      <div className="flex w-11/12 flex-col items-center justify-start space-y-4 md:w-1/2">
-        <h1 className="my-4 text-3xl font-extrabold">Rainbow Kit + SIWE</h1>
-        <div className="flex space-x-3">
+      <div className="flex w-11/12 flex-col items-center justify-start space-y-6 md:w-1/2">
+        <h1 className="text-3xl font-extrabold">
+          Rainbow Kit + SIWE (+ Magic?)
+        </h1>
+        <div className="flex w-full flex-col items-center space-y-4">
           <ConnectButton />
-          {session.status === "authenticated" && (
-            <button
-              className="whitespace-nowrap rounded-xl bg-white px-4 py-2 font-bold text-[#25292E] shadow-lg transition-transform duration-[125] hover:scale-105	"
-              onClick={() => void signOut()}
-            >
-              Sign Out
-            </button>
-          )}
+          <pre className="w-full rounded-xl bg-gray-900 p-4 text-white">
+            {JSON.stringify({ session }, null, 2)}
+          </pre>
         </div>
-        <pre className="w-full rounded-xl bg-gray-900 p-4 text-white">
-          {JSON.stringify({ session }, null, 2)}
-        </pre>
       </div>
     </div>
   );
